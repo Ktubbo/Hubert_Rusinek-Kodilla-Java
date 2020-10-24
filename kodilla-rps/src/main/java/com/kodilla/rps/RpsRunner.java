9826c5ac25f862c45ce1ec2b98c819be191487e5
Hubert Rusinek <rusinekh@gmail.com>
2020-10-24 18:31:03 +0200

    7.7 More objected way for RPS Game

--- kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
@@ -1,20 +1,17 @@
 package com.kodilla.rps;
 
-import com.kodilla.comparsion.Comparison;
 import com.kodilla.comparsion.Game;
 import com.kodilla.comparsion.Prepare;
 import com.kodilla.symbols.Symbol;
 
 import java.util.Map;
-import java.util.Scanner;
+
 
 public class RpsRunner {
 
     public static void main(String[] args) {
 
         Game game = new Game();
-        Scanner scan = new Scanner(System.in);
-        Comparison comparison = new Comparison();
         Prepare prepare = new Prepare();
         boolean doYouWantToPlayAgain = true;
         Map<Integer, Symbol> symbolMap = prepare.prepareSymbolMap();
