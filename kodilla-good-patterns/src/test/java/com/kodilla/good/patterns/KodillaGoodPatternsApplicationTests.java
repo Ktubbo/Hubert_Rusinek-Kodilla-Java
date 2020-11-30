43df7f0abcf05aaba76f50d1bf593efd442c4ee9
Hubert Rusinek <rusinekh@gmail.com>
2020-11-30 19:09:09 +0100

    13.1 Added MovieStore class

---
+++ kodilla-good-patterns/src/test/java/com/kodilla/good/patterns/KodillaGoodPatternsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kodilla.good.patterns;
+
+
+import org.junit.jupiter.api.Test;
+
+class KodillaGoodPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,6 +1,6 @@
 package com.kodilla.stream.array;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class ArrayOperationsTestSuite {
