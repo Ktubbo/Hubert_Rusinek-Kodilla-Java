5901819f23e480af2e674b451235f79b8638bfe6
Hubert Rusinek <rusinekh@gmail.com>
2020-12-08 19:44:36 +0100

    14.2 Calculator package added

--- kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -1,2 +1,17 @@
-package com.kodilla.spring.forum;public class ForumUser {
+package com.kodilla.spring.forum;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ForumUser {
+
+    String userName;
+
+    public ForumUser(){
+        this.userName="John Smith";
+    }
+
+    public String getUserName() {
+        return userName;
+    }
 }
--- kodilla-spring/src/test/java/com/kodilla/KodillaSpringApplicationTests.java
+++ kodilla-spring/src/test/java/com/kodilla/KodillaSpringApplicationTests.java
@@ -1,5 +1,7 @@
 package com.kodilla;
 
+import com.kodilla.spring.calculator.Calculator;
+import com.kodilla.spring.forum.ForumUser;
 import com.kodilla.spring.shape.Circle;
 import com.kodilla.spring.shape.Shape;
 import com.kodilla.spring.shape.Triangle;
@@ -43,6 +45,38 @@ class KodillaSpringApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testGetUsername() {
+		//Given
+		ApplicationContext context =
+				new AnnotationConfigApplicationContext("com.kodilla.spring");
+		ForumUser forumUser = context.getBean(ForumUser.class);
+
+		//When
+		String name = forumUser.getUserName();
+
+		//Then
+		assertEquals("John Smith", name);
+	}
+
+	@Test
+	void testCalculations(){
+		//Given
+		ApplicationContext context =
+				new AnnotationConfigApplicationContext("com.kodilla.spring");
+		Calculator calculator = context.getBean(Calculator.class);
+
+		//When
+		double a = 6.0;
+		double b = 1.5;
+
+		//Then
+		assertEquals(7.5,calculator.add(a,b));
+		assertEquals(4.5,calculator.sub(a,b));
+		assertEquals(9.0,calculator.mul(a,b));
+		assertEquals(4.0,calculator.div(a,b));
 	}
 
 }
